8748e168b32656a90bc54ea90e14b8b46adc5bac
bwu5 <54954733+bwu5@users.noreply.github.com>
2019-11-12 18:07:30 -0500

    update

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-    # return 'Hello World!'
+
 
 @app.route("/MBTAHelper/", methods=["GET", "POST"])
 def MBTAHelper():
     if request.method == "POST":
         location = str(request.form["location"])
+        MBTA_Station = find_stop_near(location)
 
-        if location:
-            return render_template(
-                "mbta_result.html", location = 'Boston')
+        if MBTA_Station:
+            return render_template("mbta_result.html")
         else:
             # return render_template("calculator_form.html", error=True)
             return 'ERROR!'
