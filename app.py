42e52008d23d647b9ab8600874ad9c5a36df8d57
bwu5 <54954733+bwu5@users.noreply.github.com>
2019-11-12 17:48:26 -0500

    update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -10,4 +10,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
+    # return 'Hello World!'
+
+@app.route("/MBTAHelper/", methods=["GET", "POST"])
+def MBTAHelper():
+    if request.method == "POST":
+        location = str(request.form["location"])
+
+        if location:
+            return render_template(
+                "mbta_result.html", location = 'Boston')
+        else:
+            # return render_template("calculator_form.html", error=True)
+            return 'ERROR!'
+    # return render_template("calculator_form.html", error=None)
+    return 'GET ERROR!'
