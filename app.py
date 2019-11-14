51a5e63fca587bd3b443bac48a93382575655690
dwong5 <55212715+dwong5@users.noreply.github.com>
2019-11-14 16:09:36 -0500

    Finished

--- app.py
+++ app.py
@@ -3,26 +3,19 @@ Simple "Hello, World" application using Flask
 """
 
 from flask import Flask, render_template, request
-from mbta_helper import find_stop_near
+from mbta_helper import *
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-
-@app.route("/MBTAHelper/", methods=["GET", "POST"])
+@app.route("/", methods=["GET","POST"])
 def MBTAHelper():
     if request.method == "POST":
         location = str(request.form["location"])
-        MBTA_Station = find_stop_near(location)
-
-        if MBTA_Station:
-            return render_template("mbta_result.html")
+        MBTA_Station, wheelchair = find_stop_near(location)
+        if location:
+            return render_template("mbta_result.html", location = location, MBTA_Station = MBTA_Station, wheelchair = wheelchair)
         else:
             # return render_template("calculator_form.html", error=True)
-            return 'ERROR!'
+            return render_template('index.html')
     # return render_template("calculator_form.html", error=None)
-    return 'GET ERROR!'
+    return render_template('index.html')
