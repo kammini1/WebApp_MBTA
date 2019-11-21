e4b5bcb00bf1248d01a2e2adb2af6d4c6abc088e
kammini1 <54959256+kammini1@users.noreply.github.com>
2019-11-21 18:16:16 -0500

    work

--- mbta_helper.py
+++ mbta_helper.py
@@ -46,7 +46,7 @@ def get_lat_long(place_name):
     f = urllib.request.urlopen(url)
     response_text = f.read().decode('utf-8')
     response_data = json.loads(response_text)
-    # pprint(response_data)- we don't need to show all the data
+    pprint(response_data)
     return response_data["results"][0]["locations"][0]['displayLatLng']
 
 
